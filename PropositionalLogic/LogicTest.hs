21d3acd50fadcd36cd8ef3a191e13913759d2e98
Till Theis <till_theis2@yahoo.de>
2012-07-27 02:24:35 +0200

    remove unused imports

--- PropositionalLogic/LogicTest.hs
+++ PropositionalLogic/LogicTest.hs
@@ -2,11 +2,9 @@
 
 
 import Test.Framework (defaultMain, testGroup)
-import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Test.QuickCheck
-import Test.HUnit
 
 
 import PropositionalLogic.Logic
